6b93f0b15ea38578cb90788f4fa6d46037645c40
iryne5 <irynekamau110@gmail.com>
2022-12-08 06:07:42 -0800

    fx 34 & 64 bit output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", (int) sizeof(char));
-	printf("Size of a char: %lu byte(s)\n", (int) sizeof(int));
-	printf("Size of a char: %lu byte(s)\n", (int) sizeof(long int));
-	printf("Size of a char: %lu byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a char: %lu byte(s)\n", (int) sizeof(float));
+	printf("Size of an int: %lu byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (int) sizeof(float));
 	return (0);
 }
