4e6d4fbef9ed98b0940468f2ed7115869ca8b5cf
iryne5 <irynekamau110@gmail.com>
2022-12-08 05:39:10 -0800

    rm line over 80 characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry Point
  *
- * Description: Prints exactly "Programming is like building a multilingual puzzle"
+ * Description: Prints "Programming is like building a multilingual puzzle"
  *
- * Return: Always 0 (Success)
+ * Return: Always	0	(Success)
  */
 int main(void)
 {
