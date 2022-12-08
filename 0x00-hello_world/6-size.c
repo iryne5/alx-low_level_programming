eeff2d1baa6cbdbe750036e3a2bd5b7b512d80e3
iryne5 <irynekamau110@gmail.com>
2022-12-08 05:54:07 -0800

    fix trailling whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,15 @@
-
+#include <stdio.h>
 /**
  * main - Entry Point
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
-{	
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of a char: %lu byte(s)\n", sizeof(int));
 	printf("Size of a char: %lu byte(s)\n", sizeof(long int));
