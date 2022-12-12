58bddf3baba23527f9108cd850238a95f662fac1
iryne5 <117973303+iryne5@users.noreply.github.com>
2022-12-12 16:30:30 +0300

    update 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-    int i = 122;
-    while (i > 96)
-    {
-        putchar(i);
-        i--;
-    }
+int i = 122;
+while (i > 96)
+{
+    putchar(i);
+    i--;
+}
     putchar(10);
 
     return (0);
