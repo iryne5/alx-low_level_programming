f016653ba7218ebd75c3cd8018ebe4a6c8afcd31
iryne5 <117973303+iryne5@users.noreply.github.com>
2022-12-12 16:03:13 +0300

    add main function

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,24 +3,23 @@
 /**
 * main - Entry point
 * Description - prints the alphabet in lowercase,
-* followed by a ne line
+* followed by a new line
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int i = 97;
 
 	while (i < 123)
 	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
 
-								if (i != 101 && i != 113)
-											{
-															putchar(i);
-																	}
-										i++;
-											}
-				putchar(10);
-
-					return (0);
+	return (0);
 }
-
