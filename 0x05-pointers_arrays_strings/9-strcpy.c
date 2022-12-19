e7cb58bcef5b590a250bbe4cc57d2fa4cfc21622
iryne5 <irynekamau110@gmail.com>
2022-12-19 11:03:08 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * strcpy - prints contents of an array of integers
+ * @dest: This is destiny
+ * @src: This is the copia
+ * Return: This return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
