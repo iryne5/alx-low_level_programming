30a3e39a0b32edf47533f94f247ce8f36406763c
iryne5 <irynekamau110@gmail.com>
2023-01-04 07:23:03 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,6 +7,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned in i, j;
+	unsigned int i, j;
 
-	for (j = 0
+	for (j = 0; *(s + j); j++)
+	{
+	for (i = 0; *(accept + i); i++)
+	{
+	if (*(s + j) == *(accept + i))
+	break;
+	}
+	if (*(accept + i) == '\0')
+	break;
+	}
+	return (j);
+}
