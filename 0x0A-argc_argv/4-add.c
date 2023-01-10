d147b12821d859d65323355acc320cc747a67b48
iryne5 <irynekamau110@gmail.com>
2023-01-09 23:40:36 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,9 +13,7 @@
 int check_num(char *str)
 
 {
-
 /*Declaring variables*/
-
 unsigned int count;
 
 count = 0;
@@ -28,6 +26,7 @@ return (0);
 }
 
 count++;
+}
 return (1);
 }
 
@@ -39,12 +38,10 @@ return (1);
  * Return: Always 0 (success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 
 {
-
 /*Declaring variables*/
-
 int count;
 int str_to_int;
 int sum = 0;
@@ -52,15 +49,14 @@ int sum = 0;
 count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
-	if (check_num(argv[count]))
+if (check_num(argv[count]))
 
 {
-	str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-	sum += str_to_int;
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
 }
 
 /*condition if one of the numbers contains symbols that are not digits*/
-
 else
 {
 printf("Error\n");
