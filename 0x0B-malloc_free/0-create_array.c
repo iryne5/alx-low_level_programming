9ab9ea498a33d144792f9a29e096da0ff1ddd624
iryne5 <irynekamau110@gmail.com>
2023-01-10 00:30:47 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+char *buffer;
+unsigned int position;
 
 if (size == 0)
 {
-	return (NULL);
+return (NULL);
 }
 
 /*Define values with malloc*/
@@ -21,17 +21,19 @@ buffer = (char *) malloc(size * sizeof(c));
 
 if (buffer == 0)
 {
-	return (NULL);
+return (NULL);
 }
 
 else
 {
-	position = 0;
-	while (position < size) /*while for array*/
+position = 0;
+while (position < size) /*while for array*/
 {
-	*(buffer + position) = c;
-	position++;
+*(buffer + position) = c;
+position++;
 }
 
 return (buffer);
+}
+
 }
