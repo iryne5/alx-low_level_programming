4ecd1f41c9b6bddeec2e1f79fb455d6ffe26fa4a
iryne5 <irynekamau110@gmail.com>
2023-02-03 06:04:03 -0800

    Task 1

--- 0x13-more_singly_linked_list/1-listint_len.c
+++ 0x13-more_singly_linked_list/1-listint_len.c
@@ -8,14 +8,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-		const listint_t *tp;
-			unsigned int cnr = 0;
+		size_t nodes = 0;
 
-				tp = h;
-					while (tp)
-							{
-										cnr++;
-												tp = tp->next;
-													}
-						return (cnr);
+		while (h)
+		{
+			nodes++;
+			h = h->next;
+		}
+
+		return (nodes);
 }
